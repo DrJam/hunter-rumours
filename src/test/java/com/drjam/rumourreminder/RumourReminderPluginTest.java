188b6da9fb6c3aafbb0c0724620e52a9cd8ae92e
Dan Ford <dan@fordsville.co.uk>
2024-03-24 15:51:37 +0000

    package naming confusion

--- src/test/java/com/drjam/rumourreminder/RumourReminderPluginTest.java
+++ src/test/java/com/drjam/rumourreminder/RumourReminderPluginTest.java
@@ -1,14 +1,10 @@
-package com.hunterrumours;
-
-import com.drjam.rumourreminder.RumourReminderPlugin;
+package com.drjam.rumourreminder;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class HunterRumoursPluginTest
-{
-	public static void main(String[] args) throws Exception
-	{
+public class RumourReminderPluginTest {
+	public static void main(String[] args) throws Exception {
 		ExternalPluginManager.loadBuiltin(RumourReminderPlugin.class);
 		RuneLite.main(args);
 	}
